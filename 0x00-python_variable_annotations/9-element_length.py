788e167e37d516d112e92eb4290f3b4755624b53
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-07-06 16:48:52 +0100

    Task

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""9. Let's duck type an iterable object"""
+from typing import TypeAlias, Iterable, Sequence, Tuple, List
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Duck Typing"""
+    return [(i, len(i)) for i in lst]
