b2691dba7937a73d8ab69c4d7247539ff986c33c
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-07-10 15:56:27 +0100

    Task 03

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""3. Tasks 3. Tasks 3. Tasks"""
+
+import asyncio
+import typing
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> typing.Any:
+    """Returns a asyncio.Task"""
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(wait_random(max_delay))
+    return task
