7ceb94b8dd425d197daee6a5e46ac44de17bd029
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-07-29 19:13:30 +0100

    Task 06

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,9 @@
 from client import GithubOrgClient
 import unittest
 from parameterized import parameterized
-from unittest.mock import Mock, patch, PropertyMock
+from unittest.mock import patch, PropertyMock
+from fixtures import TEST_PAYLOAD
+from typing import Callable
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -29,6 +31,22 @@ class TestGithubOrgClient(unittest.TestCase):
                    new_callable=PropertyMock, return_value=resp):
             org = GithubOrgClient('Google')
             self.assertEqual(org._public_repos_url, resp['repos_url'])
+
+    @patch('client.get_json', return_value=TEST_PAYLOAD[0][1])
+    def test_public_repos(self, p_get_json: Callable):
+        """Tests client.GithubOrgClient.public_repos"""
+
+        resp = {'repos_url': 'https://api.github.com/orgs/Google/repos'}
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock, return_value=resp)\
+                as m_org:
+            org = GithubOrgClient('Google')
+            # self.assertEqual(org._public_repos_url, resp['repos_url'])
+            self.assertEqual(org.public_repos(),
+                             ['episodes.dart', 'cpp-netlib', 'dagger', 'ios-webkit-debug-proxy',
+                             'google.github.io', 'kratu', 'build-debian-cloud', 'traceur-compiler', 'firmata.py'])
+            m_org.assert_called_once()
+            p_get_json.assert_called_once()
 
 
 if __name__ == '__main__':
